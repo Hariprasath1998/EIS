c2bc61a36ab27b5bc0ff6d4888a49c6e24b4a86f
Hariprasath1998 <50076647+Hariprasath1998@users.noreply.github.com>
2021-02-18 22:47:59 +0530

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,14 @@
 <?php 
 
-	$servername='localhost';
-	$username='Mark_42';
-	$password='enigma';
-	$dbname='EIS';
+// 	$servername='localhost';
+// 	$username='Mark_42';
+// 	$password='enigma';
+// 	$dbname='EIS';
+
+	$servername='remotemysql.com';
+	$username='51MdWQ8QKb';
+	$password='aGfBw7kyse';
+	$dbname='51MdWQ8QKb';
 	// Establish Connection to database
 	$conn=mysqli_connect($servername,$username,$password,$dbname);
 
